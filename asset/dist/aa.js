a0d882c1f44e53f0fa38678e71abf1adae6815dc
mohamedhamednour <75596573+mohamedhamednour@users.noreply.github.com>
2020-12-13 19:26:19 +0200

    first Commit

---
+++ asset/dist/aa.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(".services-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
